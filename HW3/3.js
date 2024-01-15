c8c4892317c41537192a354b987bc4c337c5a672
AAR89 <ru.booking@yandex.ru>
2024-01-15 17:35:11 +0300

    Add HW3

---
+++ HW3/3.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+Необходимо попросить пользователя ввести три числа.
+Необходимо создать функцию, в которую мы должны передать эти три числа.
+Функция должна определить максимальное, среди переданных ей значение и 
+вывести сообщение: "Максимальное значение среди чисел N1, N2, N3 равно N."
+
+Примечание: Условимся, что пользователь всегда вводит корректные значения, 
+три числа. Проверять их не нужно.
+*/
+
+const firstNumber = +prompt('Введите первое число: ');
+const secondNumber = +prompt('Введите второе число: ');
+const thirdNumber = +prompt('Введите третье число: ');
+
+function maxNumber(firstNumber, secondNumber, thirdNumber) {
+    if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+        alert(`"Макимальное значение среди чисел ${firstNumber}, ${secondNumber}, ${thirdNumber} равно ${firstNumber}`)
+    } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+        alert(`"Макимальное значение среди чисел ${firstNumber}, ${secondNumber}, ${thirdNumber} равно ${secondNumber}`)
+    } else {
+        alert(`"Макимальное значение среди чисел ${firstNumber}, ${secondNumber}, ${thirdNumber} равно ${thirdNumber}`)
+    }
+}
+
+maxNumber(firstNumber, secondNumber, thirdNumber);
