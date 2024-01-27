a9bceaa44abfa6bba1e86944a4f87f1118572adb
AAR89 <ru.booking@yandex.ru>
2024-01-27 12:57:07 +0300

    Add HW7

---
+++ HW7/1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Задание 1: ""Управление библиотекой книг""
+
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    displayInfo() {
+        console.log(`The book called "${this.title}", written by the author ${this.author}, has a ${this.pages} pages.`);
+    }
+}
+
+const newBook = new Book("Harry Potter and the Philosopher's Stone", 'Rowling Joanne', 332);
+newBook.displayInfo();
